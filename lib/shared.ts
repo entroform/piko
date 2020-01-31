e9ed6e096dd21eb1e511abbe19c8a55d3d05ba72
Andrew Prasetya <andrewprasetya@andrew-imac.local>
2020-01-31 01:29:58 -0500

    Added toArray.

--- lib/shared.ts
+++ lib/shared.ts
@@ -15,3 +15,7 @@ export function isNumber(value?: any): value is number {
 export function isNullOrUndefined(value?: any): value is null | undefined {
   return typeof value === 'undefined' || value === null;
 }
+
+export function toArray(value?: any): unknown[] {
+  return Array.isArray(value) ? value : [value];
+}
